294011a28717b170fb57d6eb6bc7ef047c7919ff
Den <dentumey@gmail.com>
2022-12-05 22:06:34 +0300

    Initial Commit

---
+++ toDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  toDoList
+//
+//  Created by Dentumei Lutfullin on 05.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
